d2fe2a73d7e4268f4114a7acb1e93e2ea134a6d0
nieshuuuu <86267663+nieshuuuu@users.noreply.github.com>
2023-04-12 09:53:16 -0700

    Update and rename Copy_of_DECT_script1.m to DECT_script.m

--- DECT_script.m
+++ DECT_script.m
@@ -1,7 +1,7 @@
 f =  waitbar(0,'preparing...','Name','QRM phantom simulation');
 n_vessels = 3;
 r_vessels = [0.25,0.15,0.05];
-m_vessels = [733,411,151;669,370,90;552,222,52];
+m_vessels = [733,411,151;669,370,90;552,222,52]; % densities
 % m_vessels = [797,101,37;403,48,32;199,41,27];
 kvp = [135,80];
 % phantom_size = 40;
@@ -9,9 +9,9 @@ phantom_size = [40,35,30];
 c = 0;
 waitbar(0,f,'generating simulation...')
 for i = 1:3
-    m = {['HA ',int2str(m_vessels(i,1)),'mg.ml'], ['HA ',int2str(m_vessels(i,3)),'mg.ml'],['HA ',int2str(m_vessels(i,2)),'mg.ml']};
+    m = {['HA ',int2str(m_vessels(i,1)),'mg.ml'], ['HA ',int2str(m_vessels(i,3)),'mg.ml'],['HA ',int2str(m_vessels(i,2)),'mg.ml']}; % create mixtures
     for s = phantom_size
-        p = Copy_of_newPhantomGene(n_vessels,r_vessels,m);
+        p = newPhantomGene(n_vessels,r_vessels,m);
         p = addFatRing(p,s,s-10);
         if s == 40
             exposure = 5.4;
