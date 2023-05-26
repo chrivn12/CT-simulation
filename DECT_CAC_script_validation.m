5985501d76beacf6cd643feadc234da2fe0d3761
Dale-Black <djblack@uci.edu>
2023-05-26 10:40:19 -0700

    Update DECT_CAC_script_validation.m
    add cleaner structure

--- DECT_CAC_script_validation.m
+++ DECT_CAC_script_validation.m
@@ -1,35 +1,131 @@
 f =  waitbar(0,'preparing...','Name','QRM phantom simulation');
 n_vessels = 3;
 r_vessels = [0.25,0.15,0.05];
-m_vessels = [733,411,151;669,370,90;552,222,52]; % densities
-% m_vessels = [797,101,37;403,48,32;199,41,27];
-kvp = [135,80];
-% phantom_size = 40;
-phantom_size = [40,35,30];
-c = 0;
+m_vessels = [15,18,22]; % densities
+m_vessels2 = [26,29,36];
+m_vessels3 = [52,59,73];
+kvp = [80, 135];
+phantom_size = [30, 35, 40];
+
+tot = length(m_vessels) * length(m_vessels2) * length(m_vessels3) * length(kvp) * length(phantom_size) * 3;
+message = sprintf('/%d images complete', tot);
+
+% run the code 3 times for 3 groups of densities
 waitbar(0,f,'generating simulation...')
-for i = 1:3
-    m = {['HA ',int2str(m_vessels(i,1)),'mg.ml'], ['HA ',int2str(m_vessels(i,3)),'mg.ml'],['HA ',int2str(m_vessels(i,2)),'mg.ml']}; % create mixtures
-    for s = phantom_size
-        p = PhantomGene(n_vessels,r_vessels,m);
-        p = addFatRing(p,s,s-10);
-        if s == 40
-            exposure = 5.4;
-            size = 'large';
-        elseif s == 35
-            exposure = 2.0;
-            size = 'medium';
-        else
-            exposure = 0.9;
-            size = 'small';
-        end
-    for energy = kvp
-        I = ToshibaAquilionOne(energy,exposure,p);
-        name = strcat('Density',int2str(i),'energy',int2str(energy),size);
-        save(name,'I');
-        c = c+1;
-        waitbar(c/18,f,strcat(int2str(c),'/18 images complete'))
+c = 0;
+m = {['HA ',int2str(m_vessels(1)),'mg.ml'], ['HA ',int2str(m_vessels(3)),'mg.ml'],['HA ',int2str(m_vessels(2)),'mg.ml']}; % create mixtures
+for s = phantom_size
+    p = PhantomGene(n_vessels,r_vessels,m);
+    p = addFatRing(p,s,s-10);
+    if s == 40
+        exposure = 5.4;
+        size = 'large';
+    elseif s == 35
+        exposure = 2.0;
+        size = 'medium';
+    else
+        exposure = 0.9;
+        size = 'small';
     end
+    
+    for energy = kvp
+        name = strcat(int2str(m_vessels(1)), "_", int2str(m_vessels(2)),  "_", int2str(m_vessels(3)),'energy',int2str(energy),size);
+
+        % generate 3 same images to simulate the differing noise for each slice.
+        I = ToshibaAquilionOne(energy,exposure,p);
+        save(strcat("val\", name, "_1"), "I");
+        c = c+1;
+        waitbar(c/tot,f,strcat(int2str(c),message))
+
+        I = ToshibaAquilionOne(energy,exposure,p);
+        save(strcat("val\", name, "_2"), "I");
+        c = c+1;
+        waitbar(c/tot,f,strcat(int2str(c),message))
+
+        I = ToshibaAquilionOne(energy,exposure,p);
+        save(strcat("val\", name, "_3"), "I");
+        c = c+1;
+        waitbar(c/tot,f,strcat(int2str(c),message))
     end
 end
+
+
+
+
+
+m = {['HA ',int2str(m_vessels2(1)),'mg.ml'], ['HA ',int2str(m_vessels2(3)),'mg.ml'],['HA ',int2str(m_vessels2(2)),'mg.ml']}; % create mixtures
+for s = phantom_size
+    p = PhantomGene(n_vessels,r_vessels,m);
+    p = addFatRing(p,s,s-10);
+    if s == 40
+        exposure = 5.4;
+        size = 'large';
+    elseif s == 35
+        exposure = 2.0;
+        size = 'medium';
+    else
+        exposure = 0.9;
+        size = 'small';
+    end
+    
+    for energy = kvp
+        name = strcat(int2str(m_vessels2(1)), "_", int2str(m_vessels2(2)),  "_", int2str(m_vessels2(3)),'energy',int2str(energy),size);
+
+        % generate 3 same images to simulate the differing noise for each slice.
+        I = ToshibaAquilionOne(energy,exposure,p);
+        save(strcat("val\", name, "_1"), "I");
+        c = c+1;
+        waitbar(c/tot,f,strcat(int2str(c),message))
+
+        I = ToshibaAquilionOne(energy,exposure,p);
+        save(strcat("val\", name, "_2"), "I");
+        c = c+1;
+        waitbar(c/tot,f,strcat(int2str(c),message))
+
+        I = ToshibaAquilionOne(energy,exposure,p);
+        save(strcat("val\", name, "_3"), "I");
+        c = c+1;
+        waitbar(c/tot,f,strcat(int2str(c),message))
+    end
+end
+
+
+
+
+m = {['HA ',int2str(m_vessels3(1)),'mg.ml'], ['HA ',int2str(m_vessels3(3)),'mg.ml'],['HA ',int2str(m_vessels3(2)),'mg.ml']}; % create mixtures
+for s = phantom_size
+    p = PhantomGene(n_vessels,r_vessels,m);
+    p = addFatRing(p,s,s-10);
+    if s == 40
+        exposure = 5.4;
+        size = 'large';
+    elseif s == 35
+        exposure = 2.0;
+        size = 'medium';
+    else
+        exposure = 0.9;
+        size = 'small';
+    end
+    
+    for energy = kvp
+        name = strcat(int2str(m_vessels3(1)), "_", int2str(m_vessels3(2)),  "_", int2str(m_vessels3(3)),'energy',int2str(energy),size);
+
+        % generate 3 same images to simulate the differing noise for each slice.
+        I = ToshibaAquilionOne(energy,exposure,p);
+        save(strcat("val\", name, "_1"), "I");
+        c = c+1;
+        waitbar(c/tot,f,strcat(int2str(c),message))
+
+        I = ToshibaAquilionOne(energy,exposure,p);
+        save(strcat("val\", name, "_2"), "I");
+        c = c+1;
+        waitbar(c/tot,f,strcat(int2str(c),message))
+
+        I = ToshibaAquilionOne(energy,exposure,p);
+        save(strcat("val\", name, "_3"), "I");
+        c = c+1;
+        waitbar(c/tot,f,strcat(int2str(c),message))
+    end
+end
+
 waitbar(1,f,'complete')
